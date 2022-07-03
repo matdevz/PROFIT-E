10428090f8a03a89bb6238bfc6e16ff1741644bd
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-03 18:10:40 -0400

    feat: Criando o header

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+	Container,
+	Logo,
+	Search,
+	SearchInput,
+	SearchIcon,
+	Nav,
+	Account,
+	AccountIcon,
+	Cart,
+	CartIcon,
+	CartQuantity,
+	Langagues,
+	IconPortuguese,
+	IconSpanish,
+	IconEnglish,
+} from './styles';
 
 function Header() {
-	return <div />;
+	return (
+		<Container>
+			<Logo>
+				<img src='./src/assets/logo-profit.svg' alt='Logo Profit-E' />
+			</Logo>
+			<Search>
+				<SearchInput type='text' placeholder='O que está procurando?' />
+				<SearchIcon />
+			</Search>
+
+			<Nav>
+				<Account>
+					<AccountIcon />
+					Minha Conta
+				</Account>
+				<Cart>
+					<CartIcon />
+					<CartQuantity>3</CartQuantity>
+				</Cart>
+
+				<Langagues>
+					<IconPortuguese>
+						<img src='./src/assets/lang-br.svg' alt='' />
+					</IconPortuguese>
+					<IconSpanish>
+						<img src='./src/assets/lang-es.svg' alt='' />
+					</IconSpanish>
+					<IconEnglish>
+						<img src='./src/assets/lang-en.svg' alt='' />
+					</IconEnglish>
+				</Langagues>
+			</Nav>
+		</Container>
+	);
 }
 
 export default Header;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,9 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
     :root{
-        --primary-color: #E5E5E5;
+        --primary-color: #FFFFFF;
         --secondary-color: #011627;
         --tertiary-color: #FF9F1C;
+        --quaternary-color: #2EC4B6;
 
         --font-title: 45px;
         --font-text: 16px
@@ -12,7 +13,7 @@ export const GlobalStyles = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        background: teal;
+        background: var(--primary-color);
         font-family:Ubuntu, Sans-Serif;
     }
 `;
